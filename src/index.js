d262bcc2550f9e55752df84c1fb811810bcc7f6b
MayronGMZ <mayron.souza@gmz.solutions>
2020-06-03 09:52:14 -0300

    Inserindo o X nos quadrados do jogo

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
+q
   render() {
     return (
-      <button className="square" onClick={() => alert('click')} >
-        { this.props.value }
+      <button
+        className="square"
+        onClick={() => this.setState({ value: 'X' })}
+      >
+        { this.state.value }
       </button>
     );
   }
